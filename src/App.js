1c19291804b67a4375b27c6297a977cb77c0639b
Sudip Sigdel <sudipsigdel2059@gmail.com>
2024-01-27 14:46:18 +0545

    Title updated

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import About from "./pages/About";
 function App() {
   return (
     <Router>
-      <Navbar title={"TextTools"} />
+      <Navbar title={"TextTool"} />
 
       <Routes>
         <Route path="/" element={<Home />} />
